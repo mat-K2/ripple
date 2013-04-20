49e940be613b2483261773f9576a1a51638d3287
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2013-04-20 15:48:38 +0900

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ripple::Application.config.secret_token = 'f37fbf15fe6bffa9cf73860c735c9e4b32a7a20211e6449d41e0674a4147a3df2d2d6f0a4e4c3915266412a2ced4114b6c1a8a0ea837e92d21798ccbe329a3ce'
