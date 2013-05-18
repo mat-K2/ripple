f2538ca4931834eed0dbe82bb3c3ba31d0ea1e35
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2013-05-18 15:02:38 +0900

    Fix bug

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,7 +2,7 @@ class EntriesController < ApplicationController
   def create
     @entry = current_user.entries.build(params[:entry])
     @entry.save
-    @entries = Service.find(params[:entry][:service_id]).entries
+    @entries = Service.find(params[:entry][:service_id]).entries.order("created_at DESC")
     respond_to do |format|
       format.js
     end
