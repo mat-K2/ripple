32f77b2f68262e8437e2b8d6671130b9480ea1e1
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2013-06-08 21:47:25 +0900

    Add feature(select language)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :authenticate_user!
+  prepend_before_filter :set_locale
+
+  def set_locale
+    I18n.locale = params[:lang] || get_locale_from_header || I18n.default_locale
+  end
+
+  private
+  def get_locale_from_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def view_select_language
+    t("views.defaults.language") + ": " +  t("views.defaults.#{I18n.locale || I18n.default_locale}")
+  end
 end
