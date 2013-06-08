5bc1cf723f77a869818e73497d718830418e0eb1
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2013-06-08 22:32:52 +0900

    Fix view(sign in)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rippler::Application.routes.draw do
-  devise_for :users, :controllers => { :registrations => "users/registrations" }
+  devise_for :users, :controllers => { :registrations => "users/registrations", :sessions => "users/sessions" }
 
   resources :services
 
