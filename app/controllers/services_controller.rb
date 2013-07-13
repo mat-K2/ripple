2948aadcaea42055f4b0acf8062ed562e742a8a6
keiyan9 <nanbakouichi@nanba-no-MacBook-Air.local>
2013-07-13 15:34:41 +0900

    Remove useless code

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -10,15 +10,5 @@ class ServicesController < ApplicationController
   def show
     @service = Service.find(params[:id])
     @make_users = @service.users
-    @entry = current_user ? current_user.entries.build : nil
-    @entries = @service.entries.order("created_at DESC")
-    url = if params[:url]
-            if params[:rippler]
-              params[:url] + "/auth/rippler?uid=#{current_user.id}"
-            else
-              params[:url]
-            end
-          end
-    @iframe_url = url
   end
 end
